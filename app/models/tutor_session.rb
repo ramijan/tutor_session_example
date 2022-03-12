0e0ebb7096eb05566aae50ac867e29ac5f361ca1
Rami Burpee <rami@scanwellhealth.com>
2022-03-12 13:19:07 -0800

    clean up sessions_for_student method

--- app/models/tutor_session.rb
+++ app/models/tutor_session.rb
@@ -1,9 +1,14 @@
 class TutorSession < ApplicationRecord
   def self.sessions_for_student(student)
-    student_session_ids = TutorSessionUser.where(kind: "Student", user_id: student.id).pluck(:tutor_session_id)
-    true_session_ids = TutorSessionUser.where(tutor_session_id: student_session_ids).order(:user_id).group_by(&:tutor_session_id).select do |k,v|
-      v.second.user_id == student.id
-    end.keys
-    where(id: true_session_ids)
+    session_ids = TutorSessionUser.where(kind: "Student", user_id: student.id).pluck(:tutor_session_id)
+
+    session_users_by_session = TutorSessionUser.where(tutor_session_id: session_ids)
+      .order(:user_id)
+      .group_by(&:tutor_session_id)
+
+    # true student session are those where student is the second user of session
+    true_session_ids = session_users_by_session.select{ |_,v| v.second.user_id == student.id }.keys
+
+    TutorSession.where(id: true_session_ids)
   end
 end
